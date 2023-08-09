8ae0c2d7d12dba5c3d388839bc3427ab0fcf4cd8
mikeleonida <leoleo@alum.mit.edu>
2023-08-09 15:34:08 -0400

    responsive side menu

--- reactchat/src/pages/templates/PrimaryAppBar.tsx
+++ reactchat/src/pages/templates/PrimaryAppBar.tsx
@@ -1,12 +1,45 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useEffect, useState } from 'react';
 
 const PrimaryAppBar = () => {
+  const [sideMenu, setSideMenu] = useState(false);
   const theme = useTheme();
+
+  const isSmallScren = useMediaQuery(theme.breakpoints.up('sm'));
+
+  useEffect(() => {
+    if (!isSmallScren && sideMenu) {
+      setSideMenu(false);
+    }
+  }, [isSmallScren]);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === keydown &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+      setSideMenu(open);
+    };
+
   return (
     <AppBar
       sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 2,
         backgroundColor: theme.palette.background.default,
         borderBottom: `1px solid ${theme.palette.divider}`,
       }}
@@ -18,6 +51,26 @@ const PrimaryAppBar = () => {
           minHeight: theme.primaryAppBar.height,
         }}
       >
+        <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+          <IconButton
+            color="inherit"
+            aria-label="open draw"
+            edge="start"
+            onClick={toggleDrawer(true)}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+
+        <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+          {[...Array(100)].map((_, i) => (
+            <Typography key={i} paragraph>
+              {i + 1}
+            </Typography>
+          ))}
+        </Drawer>
+
         <Link href="/" underline="none" color="inherit">
           <Typography
             variant="h6"
--- reactchat/src/theme/theme.tsx
+++ reactchat/src/theme/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material';
+import { createTheme, responsiveFontSizes } from '@mui/material';
 
 declare module '@mui/material/styles' {
   interface Theme {
@@ -15,6 +15,10 @@ declare module '@mui/material/styles' {
 
 export const createMuiTheme = () => {
   let theme = createTheme({
+    typography: {
+      fontFamily: ['IBM Plex Sans', 'sans-serif'].join(','),
+    },
+
     primaryAppBar: {
       height: 60,
     },
@@ -27,6 +31,7 @@ export const createMuiTheme = () => {
       },
     },
   });
+  theme = responsiveFontSizes(theme);
   return theme;
 };
 
